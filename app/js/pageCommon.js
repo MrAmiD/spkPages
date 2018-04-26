fed1ac3f259d821efe073d567fbf4508403897a7
MrAmiD <nadymov2910@gmail.com>
2018-04-26 16:10:41 +0500

    Comercial

--- app/js/pageCommon.js
+++ app/js/pageCommon.js
@@ -4,6 +4,44 @@
 
 //functions for the page will be there
 
+$('.house-slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    //dotsClass: 'slick-dots container d-flex align-items-center justify-content-end',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                dots: true
+            }
+        }
+    ],
+    prevArrow: '<button type="button" class="slick-prev sliderAboutArrow">' +
+    '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9px" height="15px"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)" d="M9.006,1.033 C8.971,1.061 8.934,1.087 8.902,1.118 C6.805,3.175 4.709,5.231 2.612,7.288 C2.580,7.319 2.549,7.352 2.508,7.395 C2.547,7.437 2.582,7.477 2.621,7.514 C4.710,9.564 6.799,11.613 8.890,13.662 C8.924,13.696 8.967,13.723 9.006,13.753 C9.006,13.783 9.006,13.812 9.006,13.842 C8.859,13.979 8.710,14.113 8.566,14.253 C8.306,14.506 8.048,14.762 7.806,15.000 C5.210,12.455 2.621,9.917 -0.005,7.343 C0.017,7.331 0.075,7.312 0.114,7.274 C2.544,4.893 4.973,2.511 7.402,0.129 C7.571,-0.037 7.972,-0.038 8.140,0.126 C8.428,0.408 8.717,0.691 9.006,0.973 C9.006,0.993 9.006,1.013 9.006,1.033 Z"/></svg>' +
+    '</button>',
+    nextArrow: '<button type="button" class="slick-next sliderAboutArrow">' +
+    '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9px" height="15px"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)" d="M-0.006,13.967 C0.029,13.939 0.066,13.913 0.097,13.881 C2.195,11.825 4.291,9.769 6.388,7.712 C6.420,7.681 6.451,7.648 6.492,7.605 C6.453,7.563 6.418,7.523 6.379,7.486 C4.290,5.436 2.201,3.387 0.110,1.338 C0.076,1.304 0.033,1.277 -0.006,1.248 C-0.006,1.218 -0.006,1.188 -0.006,1.158 C0.141,1.021 0.290,0.887 0.434,0.747 C0.695,0.494 0.952,0.239 1.194,-0.000 C3.790,2.545 6.379,5.083 9.005,7.657 C8.983,7.669 8.925,7.688 8.886,7.726 C6.456,10.107 4.027,12.489 1.598,14.871 C1.429,15.037 1.028,15.038 0.860,14.874 C0.572,14.592 0.283,14.309 -0.006,14.027 C-0.006,14.007 -0.006,13.987 -0.006,13.967 Z"/></svg>' +
+    '</button>'
+});
+
+$(document).on('click', '.js-showB', function () {
+    console.log('sssss', $(this).parent());
+
+    if($(this).hasClass('active')){
+        $(this).removeClass('active');
+        $(this).parent().next().slideUp();
+    } else {
+        $(this).addClass('active');
+        $(this).parent().next().slideDown();
+    }
+
+
+
+
+});
+
 $(function(){
-//    some scripts for this page
+
 });
